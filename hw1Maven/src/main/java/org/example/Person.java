3f8833d508a03e7e7c4cd7e360d27e70ec7849bb
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-11 23:13:59 +0300

    1hw v1

--- hw1Maven/src/main/java/org/example/Person.java
+++ hw1Maven/src/main/java/org/example/Person.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class Person {
+
     private String firstName;
     private String lastName;
     private int age;
@@ -20,14 +22,14 @@ public class Person {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
     }
 
+
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
 
 
-
-
-
-
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
 
 }
