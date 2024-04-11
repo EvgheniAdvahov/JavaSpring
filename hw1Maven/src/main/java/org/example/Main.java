57312f5c7d80df02a5d587ec7a6b93832fdf1e50
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-11 23:08:40 +0300

    1hw

--- hw1Maven/src/main/java/org/example/Main.java
+++ hw1Maven/src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Evgheni", "Advahov", 36);
+        Gson gson = new Gson();
+
+        String json = gson.toJson(person);
+        System.out.println(json);
+
+        Person person1 = gson.fromJson(json, Person.class);
+        System.out.println(person1);
+
     }
 }
---
+++ hw1Maven/src/main/java/org/example/Person.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+
+
+
+
+
+
+}
