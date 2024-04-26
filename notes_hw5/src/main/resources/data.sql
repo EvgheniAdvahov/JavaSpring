3697123eefa2013836e92c036fc12c17440b3b04
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-26 23:30:21 +0300

    files added

--- notes_hw5/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Notes (description)
-VALUES ('Сделать ДЗ');
-
-
--- INSERT INTO Notes (description, status, createdAt)
--- VALUES ('Сделать ДЗ', 'не начата', NOW());
---
--- INSERT INTO Notes (description, status, createdAt)
--- VALUES ('Поиграть', 'в процессе', NOW());
--- notes_hw5/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table Notes (
-                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                         description VARCHAR(300) NOT NULL
-);
---                          status ENUM('не начата', 'в процессе', 'завершена') NOT NULL,
---                          createdAt DATETIME
