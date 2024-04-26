ff00a67b5c240ba055cc847eee294ac92f92ac98
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-26 23:30:59 +0300

    files deleted

--- hw5_note/src/main/java/ru/gb/hw5_note/controller/NoteController.java
+++ hw5_note/src/main/java/ru/gb/hw5_note/controller/NoteController.java
@@ -17,24 +17,24 @@ public class NoteController {
     private NoteService noteService;
 
     @GetMapping
-    public List<Note> getAllNotes(){
+    public List<Note> getAllNotes() {
         return noteService.getAllNotes();
     }
 
     @PostMapping("/add")
-    public void addNote(@RequestBody Note note){
+    public void addNote(@RequestBody Note note) {
         note.setCreatedAt(new Date());
         noteService.addNote(note);
     }
 
     @PostMapping("delete/{id}")
-    public void deleteById(@PathVariable Long id){
+    public void deleteById(@PathVariable Long id) {
         noteService.deleteById(id);
     }
 
     @GetMapping("findstatus/{status}")
-    public List<Note> getNotesByStatus(@PathVariable String status){
-        if (status.equals("NOT_STARTED") || status.equals("IN_PROGRESS") || status.equals("COMPLETED")){
+    public List<Note> getNotesByStatus(@PathVariable String status) {
+        if (status.equals("NOT_STARTED") || status.equals("IN_PROGRESS") || status.equals("COMPLETED")) {
             return noteService.findNotesByStatus(Status.valueOf(status));
         } else {
             throw new IllegalArgumentException("Статус не существует: " + status);
@@ -42,10 +42,10 @@ public class NoteController {
     }
 
     @PostMapping("updatestatus")
-    public void updateStatus(@RequestParam Long id, @RequestParam String status){
-        if ( status.equals("NOT_STARTED") || status.equals("IN_PROGRESS") || status.equals("COMPLETED")){
-            if (noteService.findNoteById(id) != null){
-                noteService.updateByID(id,Status.valueOf(status));
+    public void updateStatus(@RequestParam Long id, @RequestParam String status) {
+        if (status.equals("NOT_STARTED") || status.equals("IN_PROGRESS") || status.equals("COMPLETED")) {
+            if (noteService.findNoteById(id) != null) {
+                noteService.updateByID(id, Status.valueOf(status));
             } else {
                 throw new IllegalArgumentException("Id не существует: " + id);
             }
