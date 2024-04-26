f725eaddef04ad5fffe8bee5a91b2384479d43b4
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-26 20:42:21 +0300

    files added

---
+++ hw5_note/src/test/java/ru/gb/hw5_note/Hw5NoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.hw5_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw5NoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
